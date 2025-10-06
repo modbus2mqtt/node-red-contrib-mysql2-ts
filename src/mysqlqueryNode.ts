1443e80d3fd991fc9d609f91ed44a8d4bccf4ca0
Volkmar Nissen <info@carcam360.de>
2025-10-06 12:56:43 +0200

    Initial Checkin typescript migration

---
+++ src/mysqlqueryNode.ts
@@ -0,0 +1,166 @@
+import mysql,{FieldPacket, OkPacket, Pool, ResultSetHeader, RowDataPacket} from 'mysql2/promise';
+import { Node, NodeAPI,NodeDef} from "node-red";
+import { MySQLServerNode } from './mysqlserverNode';
+
+ interface MySQLQueryNodeOptions {
+    host: string;
+    port: number;
+    database: string;
+    tls: boolean;
+    server:any;
+} 
+ interface MySQLQueryNodeDef extends NodeDef, MySQLQueryNodeOptions {}
+ class MySQLQueryNode  {
+    serverConfig: MySQLServerNode = undefined as unknown as MySQLServerNode;
+      
+    isAnObject(value:any):Boolean  {
+        return value && typeof value === 'object' && value.constructor === Object;  
+    };
+    isAnArray(value:any):Boolean  {
+        return Array.isArray(value);
+    };
+    setState(code:string, info?:string):void {
+      if (code === 'connecting') {
+        this.red().status({ fill: 'grey', shape: 'ring', text: 'connecting...' });
+      }
+      else if (code === 'connected') {
+        this.red().status({ fill: 'green', shape: 'dot', text: 'connected' });
+      }
+      else if (code === 'error') {
+        this.red().status({ fill: 'red', shape: 'ring', text: info });
+      }
+      else if (code === 'queryDone') {
+        this.red().status({ fill: 'blue', shape: 'dot', text: 'query done' });
+      }
+      else if (code === 'singleQueryDone') {
+        this.red().status({ fill: 'blue', shape: 'dot', text: 'single query done' });
+      }
+    }; 
+  resetState():void {
+      this.red().status({});
+  }
+
+  constructor(private config:MySQLQueryNodeDef) {    
+        this.red().log('Constructor called');
+        this.red().on('input',  async ( msg:any) => {
+      if (typeof(msg.topic) !== 'string' || !msg.topic) {
+        this.red().error('msg.topic should be a string containing the SQL query.');
+        return;
+      }
+     if (msg.payload !== undefined && !this.isAnObject(msg.payload)) {
+        this.red().error(
+          "msg.payload should be an object or an array containing the query arguments."
+        );
+        return false;
+      }
+      
+      if ((msg.payload != undefined ))
+        if( !Array.isArray(msg.payload)){
+          this.serverConfig
+            .query(
+              msg.topic,
+              msg.payload
+            ).then( ([result, fields]) => {
+              this.setState('singleQueryDone');
+              msg.payload = result;
+              this.red().send(msg);
+            }).catch( error => {
+              this.red().error(error, msg);
+              this.setState('error', error.toString());
+            });
+      }
+      else 
+        this.processArray(msg);
+      else{
+          //this.error(error, "No payload passed");
+          this.setState('error', "Error no payload");
+        }
+    });
+    this.red().on('close', () => {  
+      this.serverConfig.red().removeAllListeners();
+      this.resetState();
+    });
+
+    this.serverConfig.connect();
+
+  }
+    red():Node{
+        return this as unknown as Node;
+    }
+    private processArray( msg:any):void{
+     let values = [];
+     values =msg.payload;
+     if( !this.serverConfig || !this.serverConfig.pool){
+        this.red().error("No database connection");
+        this.setState('error', "No database connection");
+        return;
+     }
+      this.serverConfig.pool.getConnection()
+        .catch((err) => {
+         let msg = "Error getting connection from pool: " + err;
+         this.red().error(msg);
+         this.setState('error', msg);
+         return;
+        }).then((connection) => {
+          if(!connection){
+            this.red().error("No connection available");
+            this.setState('error', "No connection available");
+            return;
+          }
+          connection.beginTransaction().then( ()=>{
+            let promisses:Promise<[RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader, FieldPacket[]]>[] = [];
+            msg.payload = values.forEach((value:any) => {
+              promisses.push(
+                  connection.query(
+                    msg.topic,
+                    value
+              )
+              );
+            });
+            Promise.all(promisses).then((results) => {
+             connection.commit().then(() =>   {
+                    connection.release();
+                    msg.payload = results;
+                    this.setState("queryDone");
+                    this.red().send(msg);
+                  })
+                    .catch((error) => {     
+                      connection.rollback().then(()=> {
+                      connection.release();
+                      //Failure
+                      let msg = "Error committing transaction: " + error;
+                      this.red().error(msg );
+                      this.setState("error", msg);
+                    });
+                  })
+                })
+              .catch((error) => {
+              connection.rollback() .then(()=>  {
+                connection.release();
+              //Failure
+                let msg = "Error execute query: " + error;
+                this.red().error(msg);
+                this.setState("error", msg);
+              });
+          });
+          }).catch((error) => {
+            let msg = "Error: getConnection from pool" + error;
+            //Failure
+            this.red().error(msg);
+            this.setState("error", msg);
+          })
+      });
+  }
+}
+
+export = function(RED:NodeAPI){
+
+  function MySQLQueryNodeCreate(this:Node, config:MySQLQueryNodeDef) {
+    let thisNode:MySQLQueryNode= new MySQLQueryNode(config)
+    RED.nodes.createNode(thisNode.red(), config);
+    thisNode.serverConfig = RED.nodes.getNode(config.server) as unknown as MySQLServerNode;
+  }
+  RED.nodes.registerType(
+        'MySQL-Query',
+        MySQLQueryNodeCreate);
+}
---
+++ src/mysqlserverNode.ts
@@ -0,0 +1,119 @@
+import mysql,{Pool} from 'mysql2/promise';
+import { Node, NodeAPI,NodeDef} from "node-red";
+
+ interface MySQLServerNodeOptions {
+    host: string;
+    port: number;
+    database: string;
+    tls: boolean;
+ } 
+ interface MySQLServerNodeDef extends NodeDef, MySQLServerNodeOptions {}
+ interface MySQLServerNodeCredentials { user: string; password: string; }
+ export class MySQLServerNode  {
+   pool: Pool|null = null ;
+   pingTimeout: NodeJS.Timeout|null = null;
+   credentials: { user: string; password: string; } = { user: '', password: '' };
+
+    query(sql: string, values?: any):Promise<any>{
+        if( this.pool)
+          return this.pool.query(sql, values);
+        throw new Error('Not connected'); 
+    };
+    pingReset(timeout?: number):void{
+      if (this.pingTimeout) {
+        clearTimeout(this.pingTimeout);
+      }
+      if(timeout!= undefined)
+        this.pingTimeout = setTimeout(this.ping, timeout * 1000);
+    };
+    ping():Promise<void>{
+        return  new Promise( (resolve, reject) => {
+        this.query('SELECT version();').then( values => {
+          this.red().emit('state', 'connected');
+          this.pingReset(); 
+            resolve();
+        }).catch( error => {
+          this.red().emit('state', 'error',  error.toString());
+          this.pingReset(5);
+          reject(error);
+        });
+        });
+    };
+
+  connect():Promise<void> {
+    return new Promise( (resolve, reject) => {
+      this.red().emit('state', 'connecting');
+
+      if (this.pool) {
+        resolve();
+      }
+
+      // Note: the connection is not done here
+      this.pool = mysql.createPool({
+        host: this.config.host,
+        port: this.config.port,
+        user: this.credentials.user,
+        password: this.credentials.password,
+        database: this.config.database,
+        waitForConnections: true,
+        connectionLimit: 5,
+        queueLimit: 0,
+        connectTimeout: 1000,
+        
+        ssl: this.config.tls ? {} : undefined,
+
+        // See https://www.npmjs.com/package/mysql#custom-format
+        queryFormat: (query, values) => {
+          if (!values) return query;
+          return query.replace(/:(\w+)/g, (txt, key) => {
+            return Object.prototype.hasOwnProperty.call(values, key)
+              ? mysql.escape(values[key])
+              : txt;
+          });
+        }
+      })
+
+      // Do a ping that will trigger the connection and check that it is working
+    return this.ping();
+    })
+    
+}
+   constructor(private config:MySQLServerNodeDef) {    
+        this.red().log('Constructor called');
+        this.red().on('close',  (done:()=>void) => {
+                if (this.pingTimeout) {
+                    clearTimeout(this.pingTimeout);
+                }
+        if (this.pool) {
+                this.pool.end().finally( () => {
+                    this.pool = null;
+                    this.red().emit('state');
+                    done();
+                });
+            }
+        });
+    }
+
+    red():Node<MySQLServerNodeCredentials>{
+        return this as unknown as Node<MySQLServerNodeCredentials>;
+    }
+}
+
+export default function(RED:NodeAPI){
+
+  function mySQLServerNodeCreate(this:Node<MySQLServerNodeCredentials>, config:MySQLServerNodeDef) {
+    let thisNode:MySQLServerNode= new MySQLServerNode(config)
+    RED.nodes.createNode(thisNode.red(), config);
+  }
+  RED.nodes.registerType(
+        'MySQL-Server',
+        mySQLServerNodeCreate,
+  {
+      credentials: {
+        user: { type: 'text' },
+        password: { type: 'password' }
+      }
+  });
+}
+
+  
